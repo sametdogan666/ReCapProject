8033596324aec0f993e8f3481e05a63576892a43
sametdoqann <sametdoqann-s.doqann1570@gmail.com>
2021-03-02 00:09:47 +0300

    2 Mart Düzeltme

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,66 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Business;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        ICarImageDal _carImageDal;
+
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _carImageDal = carImageDal;
+        }
+
+        public IResult Add(CarImage carImage)   
+        {
+            var result = BusinessRules.Run(CheckImageLimit(carImage.CarId));
+            if (result!=null)
+            {
+                return result;
+            }
+            _carImageDal.Add(carImage);
+            return new SuccessResult(Messages.CarImageAdded);
+        }
+       
+
+        public IResult Delete(CarImage carImage)
+        {
+            _carImageDal.Delete(carImage);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            return new SuccessDataResult<List<CarImage>>(_carImageDal.GetAll());
+        }
+
+        public IDataResult<CarImage> GetById(int id)
+        {
+            return new SuccessDataResult<CarImage>(_carImageDal.Get(c => c.CarId == id));
+        }
+
+        public IResult Update(CarImage carImage)
+        {
+            _carImageDal.Update(carImage);
+            return new SuccessResult();
+        }
+
+        private IResult CheckImageLimit(int carId)
+        {
+            var context = _carImageDal.GetAll(c => c.CarId == carId).Count;
+            if (context > 5)
+            {
+                return new ErrorResult();
+            }
+            return new SuccessResult();
+        }
+    }
+}
